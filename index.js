c25103c9fc6a22a63480d33f26a93c71c1495ac9
awgore99 <awgore99@gmail.com>
2022-11-28 11:00:28 -0600

    Added questions and requirements

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    // Runs each question through inquirer
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the title of your application?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the description of your application?",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What is the install process for your application?",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "How do you use your application?",
+            name: "usage",
+        },
+        {
+            type: "checkbox",
+            message: "What License did you use for your application?",
+            choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0"],
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "How can people contribute to the project?",
+            name: "contributing",
+        },
+        {
+            type: "input",
+            message: "What are the instructions for testing?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username",
+        },
+        {
+            type: "input",
+            message: "What is your email accociated with your GitHub?",
+            name: "email"
+        },
+    ])
+        .then(input => {
+            return input;
+        });
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
