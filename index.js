f2a4b2900d0c354093ee11c3b7d3600de5d0ee66
awgore99 <awgore99@gmail.com>
2023-01-02 23:53:51 -0600

    Added init logic

--- index.js
+++ index.js
@@ -6,58 +6,58 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
     // Runs each question through inquirer
-        {
-            type: "input",
-            message: "What is the title of your application?",
-            name: "title",
-        },
-        {
-            type: "input",
-            message: "What is the description of your application?",
-            name: "description",
-        },
-        {
-            type: "input",
-            message: "What is the install process for your application?",
-            name: "installation",
-        },
-        {
-            type: "input",
-            message: "How do you use your application?",
-            name: "usage",
-        },
-        {
-            type: "checkbox",
-            message: "What License did you use for your application?",
-            choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0"],
-            name: "license",
-        },
-        {
-            type: "input",
-            message: "How can people contribute to the project?",
-            name: "contributing",
-        },
-        {
-            type: "input",
-            message: "What are the instructions for testing?",
-            name: "tests"
-        },
-        {
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "username",
-        },
-        {
-            type: "input",
-            message: "What is your email accociated with your GitHub?",
-            name: "email"
-        },
-    ];
+    {
+        type: "input",
+        message: "What is the title of your application?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the description of your application?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What is the install process for your application?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "How do you use your application?",
+        name: "usage",
+    },
+    {
+        type: "checkbox",
+        message: "What License did you use for your application?",
+        choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0"],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "How can people contribute to the project?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "What are the instructions for testing?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email accociated with your GitHub?",
+        name: "email"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFile(fileName, data, err => {
-        if(err){
+        if (err) {
             reject(err)
             return;
         }
@@ -65,7 +65,15 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    try {
+        const responses = inquirer.prompt(questions);
+        const markdown = generateMarkdown(responses);
+        writeToFile('ReadME.md', markdown);
+    } catch (err) {
+        console.log(error);
+    }
+}
 
 // Function call to initialize app
 init();
